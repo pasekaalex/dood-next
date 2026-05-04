a91965f232fafec2d0a8a002567900d00047bae4
alex <alex@bulk.game>
2026-05-04 14:29:51 -0400

    25px bottom padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import ContractAddress from './components/ContractAddress';
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <div className="w-full max-w-xl mx-auto flex flex-col gap-3 pb-2">
+      <div className="w-full max-w-xl mx-auto flex flex-col gap-3" style={{paddingBottom: '25px'}}>
         <Hero />
         {/* Slideshow placeholder - coming soon */}
         <PFPSection />
